02f5201d15b2fe103cf343efc2bb9275f709cb07
Solirs <zcauchemar@gmail.com>
2022-07-16 17:36:24 +0200

    Move code to if __name__ == "__main__" as it's a general good practice

--- quickalias.py
+++ quickalias.py
@@ -11,53 +11,53 @@ user_directory: str = os.path.expanduser('~')
 # Getting the process id of the parent process.
 process_id: str = os.readlink(f'/proc/{os.getppid()}/exe')
 
-if "bash" in process_id:
-    SHELL = "bash"
-    # Getting the path of the bashrc.
-    shell_config_path: str = os.path.join(user_directory, '.bashrc')
+if __name__ == "__main__":
+    if "bash" in process_id:
+        SHELL = "bash"
+        # Getting the path of the bashrc.
+        shell_config_path: str = os.path.join(user_directory, '.bashrc')
 
-elif "zsh" in process_id:
-    SHELL = "zsh"
-   # Getting the path of the .zshrc file.
-    shell_config_path: str = os.path.join(user_directory, '.zshrc')
-elif "fish" in process_id:
-    SHELL = "fish"
-    # Getting the path of the config.fish file.
-    shell_config_path: str = os.path.join(
-        user_directory, '.config/fish/config.fish')
-else:
-    # If the shell is not detected, it will default to fish.
-    SHELL = "fish"
-    print("Shell not detected. Defaulting to fish.")
-    shell_config_path: str = None
+    elif "zsh" in process_id:
+        SHELL = "zsh"
+    # Getting the path of the .zshrc file.
+        shell_config_path: str = os.path.join(user_directory, '.zshrc')
+    elif "fish" in process_id:
+        SHELL = "fish"
+        # Getting the path of the config.fish file.
+        shell_config_path: str = os.path.join(
+            user_directory, '.config/fish/config.fish')
+    else:
+        # If the shell is not detected, it will default to fish.
+        SHELL = "fish"
+        print("Shell not detected. Defaulting to fish.")
+        shell_config_path: str = None
 
-if shell_config_path is not None:
-    config_location: str = shell_config_path
-else:
-    config_location: str = f"{user_directory}/.config/fish/config.fish"
+    if shell_config_path is not None:
+        config_location: str = shell_config_path
+    else:
+        config_location: str = f"{user_directory}/.config/fish/config.fish"
+    # Asking the user to input the alias and the command.
+    alias: str = input('enter alias for command: ')
+    command: str = input('enter the command: ')
 
-# Asking the user to input the alias and the command.
-alias: str = input('enter alias for command: ')
-command: str = input('enter the command: ')
+    if SHELL in "bash" or SHELL in "zsh":
+        alias_string: str = f"alias {alias}=\"{command}\""
 
-if SHELL in "bash" or SHELL in "zsh":
-    alias_string: str = f"alias {alias}=\"{command}\""
+    else:
+        alias_string: str = f"alias {alias} \"{command}\""
 
-else:
-    alias_string: str = f"alias {alias} \"{command}\""
+    # This is checking if the alias already exists in the config file.
+    # if it does, it will not add it again.
+    with open(config_location, encoding="utf-8") as f:
+        if alias_string in f.read():
+            print(f"{alias} already exists in {config_location}")
+            sys.exit(0)
 
-# This is checking if the alias already exists in the config file.
-# if it does, it will not add it again.
-with open(config_location, encoding="utf-8") as f:
-    if alias_string in f.read():
-        print(f"{alias} already exists in {config_location}")
-        sys.exit(0)
+    # Opening the config file in append mode and writing the alias to the file.
+    with open(config_location, 'a', encoding="utf-8") as f:
+        f.write(f"{alias_string}\n")
 
-# Opening the config file in append mode and writing the alias to the file.
-with open(config_location, 'a', encoding="utf-8") as f:
-    f.write(f"{alias_string}\n")
+    print(f"added \"{alias_string}\" to shell config")
 
-print(f"added \"{alias_string}\" to shell config")
-
-source_command: str = f"source {config_location}"
-print(f"You can source the new changes with:\n\t{source_command}")
+    source_command: str = f"source {config_location}"
+    print(f"You can source the new changes with:\n\t{source_command}")
